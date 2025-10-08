0442d63dfcadaae15d425b3367247f1177fe3a12
Omkar Bharathi <omkarbharathi94@gmail.com>
2025-10-08 15:17:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+os.environ['FACE_RECOGNITION_MODELS'] = r'C:\Users\omkar\OneDrive\Documents\Project\face_recognition_models'
+
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import csv
+
+# Folder to store registered face images
+path = 'TrainingImages'
+images = []
+classNames = []
+myList = os.listdir(path)
+
+# Load images and class names
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encodes = face_recognition.face_encodings(img)
+        if encodes:
+            encodeList.append(encodes[0])
+    return encodeList
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%Y-%m-%d %H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDist = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDist)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, name, (x1+6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            markAttendance(name)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
